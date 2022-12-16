0d1f134454fcbc07e5eed08809d754fe08c9c2f9
Guille A <LGuille.2000@gmail.com>
2022-12-16 04:05:03 -0300

    up

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const { match } = require('assert');
 const express = require('express')
 const fs = require('fs')
+const { send } = require('process')
+const { Router } = express
 //express
 const app = express()
 const PORT = process.env.PORT || 8080
-
+//router
+const productsRouter = new Router()
 
 //db Control
 class Contenedor {
@@ -47,17 +49,77 @@ class Contenedor {
         } catch (err) {console.log(`error => ${err}`)}
     }
     
+    getAddProduct (product) {
+        try {
+            let products = this.getAll()
+            products.push(product)
+            fs.writeFileSync(this.file, JSON.stringify(products))
+        } catch (err) {console.log(`error => ${err}`)}
+    }
+
+    getDeleteId (id) {
+        try {
+            let products = this.getAll()
+            products.forEach(product => {
+                if (product === id) {
+                    product = {}
+                }
+            });
+        } catch (err) {console.log(`error => ${err}`)}
+    }
 }
 const container = new Contenedor('./db/products.txt')
+
 //get
-app.get('/products', (req, res) => {
+productsRouter.get('/', (req, res) => {
     res.send(container.getAll())
 })
-app.get('/productRandom', (req, res) => {
-    let random = Math.floor(Math.random() * 3)
-    console.log(random)
-    res.send(container.getById(random))
+productsRouter.get('/:id', (req, res) => {
+    let productSearch = req.params
+    res.send(container.getById(productSearch))
 })
+//post
+productsRouter.post('/', (req, res) => {
+    let product = req.body;
+    if (product == undefined) {
+        res.send({"error": `The product is not defined`})
+    } else {
+        res.send(container.getAddProduct(product))
+    }
+})
+//put
+productsRouter.put('/:id', (req, res) => {
+    let search = req.params
+    let newProduct = req.body
+    if (container.getById(search) == undefined || container.getById(search) == null) {
+        res.send({"error": `The product ${search} is not defined`})
+    } else {
+        if (newProduct == undefined || newProduct == null) {
+            res.send({"error": `The product updated is not defined`})
+        } else {
+            container.getDeleteId(search)
+            container.getAddProduct(newProduct)
+            res.send({"ok": `The product updated ${newProduct}`})
+        }
+    }
+})
+
+//delete
+productsRouter.delete('/:id', (req, res) => {
+    let search = req.params
+    if (container.getById(search) == undefined || container.getById(search) == null) {
+        res.send({"error": `The product ${search} is not defined`})
+    } else {
+        container.getDeleteId(search)
+        res.send({"ok": `The product delete ${search}`})
+    }
+})
+
+//app use
+app.use('/api/products', productsRouter)
+app.use('/api/form', express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended: true}))
+
 
 //server
 const server = app.listen(PORT, () => {
